b1e4f13d3a48ed28d28d0611c2e17e2d1b3de90b
GeistInDerSH <icomedy.us@gmail.com>
2020-08-22 13:25:10 -0700

    Added functions for handeling different web pages. Added anonymus function to run every 12 hours to update covid data

--- main.go
+++ main.go
@@ -3,21 +3,72 @@ package main
 import (
 	"github.com/GeistInDerSH/Covid19-Watch/covid_data"
 	"github.com/GeistInDerSH/Covid19-Watch/csv_data"
+	"github.com/gorilla/mux"
 
+	"encoding/json"
 	"log"
-	"sort"
+	"net/http"
+	"strings"
+	"time"
 )
 
-// sortResults sorts the covid data based on the number of deaths from highest to lowest
-func sortResults(covidData []*covid.CombinedCovidData) []*covid.CombinedCovidData {
-	sort.Slice(covidData[:], func(i, j int) bool {
-		return covidData[i].Deaths > covidData[j].Deaths
-	})
+var (
+	MergedData []*covid.CombinedData
+)
 
-	return covidData
+// returnAllCounties returns a json list containing all countries, their
+// confirmed cases, deaths, and recoveries.
+func returnAllCountries(w http.ResponseWriter, r *http.Request) {
+
+	if MergedData == nil {
+		updateCovidData()
+	}
+
+	json.NewEncoder(w).Encode(MergedData)
 }
 
-func main() {
+// returnSingleCountry checks for the given country matching the id.
+// If it is found, the json data will be written to the page.
+func returnSingleCountry(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	if MergedData == nil {
+		updateCovidData()
+	}
+
+	// Use the 2 letter country code
+	if len(key) == 2 {
+		key = strings.ToUpper(key)
+		for _, country := range MergedData {
+			if country.Id == key {
+				json.NewEncoder(w).Encode(country)
+				break
+			}
+		}
+	} else {
+		for _, country := range MergedData {
+			if country.Country == key {
+				json.NewEncoder(w).Encode(country)
+				break
+			}
+		}
+	}
+}
+
+// handleRequests sets up the different web pages and the functions
+// associated with those pages.
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/countries", returnAllCountries)
+	router.HandleFunc("/country/{id}", returnSingleCountry)
+	log.Fatal(http.ListenAndServe(":80", router))
+}
+
+// updateCovidData retreives the confirmed cases, deaths, and recovered
+// cases for each of the countries. These three are then merged together.
+func updateCovidData() {
 	confirmedData, err := csvdata.GetCSVData("confirmed")
 	if err != nil {
 		log.Fatal("Could not download confirmed data")
@@ -33,14 +84,24 @@ func main() {
 		log.Fatal("Could not download recovered data")
 	}
 
-	mergedData, err := covid.MergeResults(confirmedData, deathData, recoveredData)
-	if err != nil {
-		log.Fatal("Could not merge the data")
-	}
+	MergedData = covid.MergeResults(confirmedData, deathData, recoveredData)
+}
 
-	mergedData = sortResults(mergedData)
+func main() {
+	ticker := time.NewTicker(12 * time.Hour)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				updateCovidData()
 
-	for _, d := range mergedData {
-		d.PrintData()
-	}
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	handleRequests()
 }
